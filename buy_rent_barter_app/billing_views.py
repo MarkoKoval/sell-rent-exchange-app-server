73a4d7c028db3b5c689201c176e28d938ac962e4
Your Name <markblacksmith1999@gmail.com>
2020-04-27 02:39:50 +0300

    mess

---
+++ buy_rent_barter_app/billing_views.py
@@ -0,0 +1,40 @@
+from .liqp import LiqPay
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView, View
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+import json
+
+@csrf_exempt
+def PayView(request, *args, **kwargs):
+        template_name = 'pay.html'
+        liqpay = LiqPay("i14834950111", "MOd35a45stEweq6tlDeY83HmPkCGe8nbyCdZ9vEV")
+        params = {
+
+            'action': 'paydonate',
+            'amount': '1',
+            'currency': 'UAH',
+            'description': 'Donate',
+            'version': '3',
+            'sandbox': 0, # sandbox mode, set to 1 to enable it
+            'server_url': '127.0.0.1:8000/pay-callback/', # url to callback view
+        }
+        signature = liqpay.cnb_signature(params)
+        data = liqpay.cnb_data(params)
+        return JsonResponse(json.dumps({'signature': signature, 'data': data}), content_type="application/json", safe=False)
+        #return render(request, template_name, {'signature': signature, 'data': data})
+
+
+@csrf_exempt
+def PayCallbackView(request, *args, **kwargs):
+        liqpay = LiqPay("i14834950111", "MOd35a45stEweq6tlDeY83HmPkCGe8nbyCdZ9vEV")
+        data = request.POST.get('data')
+        signature = request.POST.get('signature')
+        sign = liqpay.str_to_sign("MOd35a45stEweq6tlDeY83HmPkCGe8nbyCdZ9vEV" + data + "MOd35a45stEweq6tlDeY83HmPkCGe8nbyCdZ9vEV")
+        print(sign)
+        print(signature)
+        if sign == signature:
+            print('callback is valid')
+        response = data
+        print('callback data', response)
+        return HttpResponse()
--- buy_rent_barter_app/urls.py
+++ buy_rent_barter_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import *
+from .billing_views import *
+
 urlpatterns = [
-    path("upload_image/", list1)
+    path("", PayView, name='pay_view'),
+    path("upload_image/", list1),
+    path('pay-callback/', PayCallbackView, name='pay_callback')
 ]
